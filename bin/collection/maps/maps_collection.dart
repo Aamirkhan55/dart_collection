8683a18a295201cc84dd57de7869cc1e2be8a835
Amir55143 <aamirnoor55143@gmail.com>
2023-12-25 11:13:45 +0500

    Maps

--- bin/collection/maps/maps_collection.dart
+++ bin/collection/maps/maps_collection.dart
@@ -1,3 +1,12 @@
 void main () {
-  
+// In general, a map is an object that associates keys and values. 
+//Both keys and values can be any type of object.
+ Map<String, String> colors = {
+    'apple': 'red',
+    'banana': 'yellow',
+    'grape': 'purple',
+  };
+  print(colors);
+  colors['peach'] = 'yellow + rwd';
+  print(colors);
 }
